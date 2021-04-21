728d357d12e6373f2984af88c90979120594687a
Carlos Severo <severo.e.carlos@gmail.com>
2021-04-21 13:56:11 -0300

    added connection migration and repository

---
+++ server/src/database/migrations/1619019552967-CreateConnections.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateConnections1619019552967 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: "connections",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "admin_id",
+            type: "uuid",
+            isNullable: true,
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKConnection",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
---
+++ server/src/entities/Connections.ts
@@ -0,0 +1,41 @@
+import {
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+import { User } from "./Users";
+
+@Entity("connections")
+class Connection {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @JoinColumn({ name: "user_id" })
+  @OneToOne(() => User)
+  user: User;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Connection };
